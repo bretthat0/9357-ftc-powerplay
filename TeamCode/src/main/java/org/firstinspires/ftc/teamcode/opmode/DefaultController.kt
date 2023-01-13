64dc6960965f0926bda8899511be3485912c81e2
Tyler Bretthauer <bretthat@wlhs.wlwv.k12.or.us>
2023-01-13 15:06:01 -0800

    pushed changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DefaultController.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DefaultController.kt
@@ -7,13 +7,15 @@ import com.qualcomm.robotcore.hardware.Gamepad
 import org.firstinspires.ftc.teamcode.subsystem.ArmSubsystem
 import org.firstinspires.ftc.teamcode.subsystem.BallDriveSubsystem
 import org.firstinspires.ftc.teamcode.util.vec3
+import kotlin.math.max
+import kotlin.math.min
 
 @TeleOp(name = "Default Controller")
 class DefaultController: ControllerBase() {
     private lateinit var driveSubsystem: MecanumDriveSubsystem
     private lateinit var armSubsystem: ArmSubsystem
 
-    private var isGrabbing = false
+    private var speed = (MAX_SPEED + MIN_SPEED) / 2.0
 
     override fun onInit() {
         driveSubsystem = MecanumDriveSubsystem(hardwareMap)
@@ -27,42 +29,61 @@ class DefaultController: ControllerBase() {
 
     override fun onLoop() {
         drive()
-        arm();
+        arm()
 
         telemetry.addLine("Game harder >:(")
+        //driveSubsystem.doTelemetry(telemetry)
         armSubsystem.doTelemetry(telemetry)
         telemetry.update()
     }
 
     private fun drive() {
-        driveSubsystem.leftInput = gamepad1.leftStick
-        driveSubsystem.rightInput = gamepad1.rightStick
+        driveSubsystem.leftInput = gamepad1.leftStick * speed
+        driveSubsystem.rightInput = gamepad1.rightStick * speed
     }
 
     private fun arm() {
         //armSubsystem.position = vec3(0.0, 10.0, 10.0)
 
         if (gamepad2.y) {
-            armSubsystem.rotatePosition += gamepad2.triggerAxis * INP_SPEED * deltaTime
-            armSubsystem.wristPosition += gamepad2.bumperAxis * 0.01 * deltaTime
+            armSubsystem.rotatePosition += gamepad2.triggerAxis * INPUT_DELTA * deltaTime
+            armSubsystem.wristPosition += gamepad2.bumperAxis * INPUT_DELTA * deltaTime
         }
         else {
-            armSubsystem.extendPosition += gamepad2.triggerAxis * INP_SPEED * deltaTime
-            armSubsystem.pivotPosition += gamepad2.bumperAxis * INP_SPEED * deltaTime
+            armSubsystem.extendPosition += gamepad2.triggerAxis * INPUT_DELTA * deltaTime
+            armSubsystem.pivotPosition += gamepad2.bumperAxis * INPUT_DELTA * deltaTime
         }
-
-        armSubsystem.isGrabbing = isGrabbing
     }
 
     override fun onButtonReleased(gamepad: Gamepad, button: GamepadButton) {
-        if (gamepad == gamepad2) {
-            if (button == GamepadButton.A) {
-                isGrabbing = !isGrabbing
+        when (gamepad) {
+            gamepad1 -> {
+                when (button) {
+                    GamepadButton.DPAD_UP -> min(speed + SPEED_STEP, MAX_SPEED)
+                    GamepadButton.DPAD_DOWN -> max(speed - SPEED_STEP, MIN_SPEED)
+                }
+            }
+            gamepad2 -> {
+                when (button) {
+                    GamepadButton.A -> armSubsystem.isGrabbing = !armSubsystem.isGrabbing
+                    GamepadButton.RS -> {
+                        when (armSubsystem.mode) {
+                            ArmSubsystem.Mode.WorldSpace -> ArmSubsystem.Mode.Manual
+                            ArmSubsystem.Mode.Manual -> ArmSubsystem.Mode.WorldSpace
+                        }
+                    }
+                    // TODO: Toggle between arm/turret/claw
+                    // TODO: Dpad Preset Arm Positions
+                }
             }
         }
     }
 
     companion object {
-        const val INP_SPEED = 0.12
+        const val INPUT_DELTA = 0.12
+        const val SPEED_STEP = 0.15
+
+        const val MAX_SPEED = 1.0
+        const val MIN_SPEED = 0.1
     }
 }
